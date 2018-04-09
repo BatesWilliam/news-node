6a7434b76a09963a9d4f1f44b70500d51384ca34
William Bates <wb1009200@wtstudents.wtamu.edu>
2018-04-09 14:13:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lalfaloujeh/myapplication/database.java
@@ -0,0 +1,64 @@
+package com.example.lalfaloujeh.myapplication;
+
+
+
+public class database  {
+    public static final class PoliticsTable {
+        public static final String NAME = "NewsPoliticsScheme ";
+
+        public static final class Cols {
+            public static final String UUID = "uuid"; //primary key
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String Author = "Author";
+            public static final String link = "link";
+            public static final String Description  = "Description";
+            public static final String Thumbnail = "Thumbnail";
+        }
+    }
+    public static final class SportsNews {
+        public static final String NAME = "NewsSportsScheme ";
+
+        public static final class Cols {
+            public static final String UUID = "uuid"; //primary key
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String Author = "Author";
+            public static final String link = "link";
+            public static final String Description  = "Description";
+            public static final String Thumbnail = "Thumbnail";
+        }
+    }
+
+    public static final class CelebrityNews {
+        public static final String NAME = "NewsCelebrityScheme ";
+
+        public static final class Cols {
+            public static final String UUID = "uuid"; //primary key
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String Author = "Author";
+            public static final String link = "link";
+            public static final String Description  = "Description";
+            public static final String Thumbnail = "Thumbnail";
+        }
+    }
+    public static final class EconomicsNews {
+        public static final String NAME = "NewsEconomicsScheme ";
+
+        public static final class Cols {
+            public static final String UUID = "uuid"; //primary key
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String Author = "Author";
+            public static final String link = "link";
+            public static final String Description  = "Description";
+            public static final String Thumbnail = "Thumbnail";
+        }
+    }
+
+
+
+
+
+}
